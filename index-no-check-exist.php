3deab307ba48b3d18d34d9da1a8cdbd9ccf2a4a5
QuocVo <quoc.vo@gotit.vn>
2023-11-06 17:18:07 +0700

    init

---
+++ index-no-check-exist.php
@@ -0,0 +1,35 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+//include __DIR__ . '/src/Support/helpers-no-check-exist.php';
+
+use TrueMe\Support\Arr;
+
+/////////////////////////////////////////
+
+echo '-------------------------------------------------' . '</br>';
+echo '|Case1 ----------------------------------------' . '</br>';
+echo '|__+ NO check existing function--' . '</br>';
+echo '-----------------------------------------------' . '</br>';
+echo 'Step1---------';
+$m1 = memory_get_usage();
+echo $m1;
+echo '</br>';
+
+
+$appendConfig = append_config([1,2,5,4,3]);
+
+
+echo 'Step2---------';
+$m2 = memory_get_usage();
+echo $m2;
+echo '</br>';
+
+echo '---------------';
+echo '</br>';
+echo 'Used: ' . ($m2-$m1);
+echo '</br>';
+echo '----------------------------------------' . '</br>';
+
+var_dump('$appendConfig', $appendConfig);
+
